74023553f8e4c68a46f02fae3bfc7b4e710abb4f
Daniel Lepiorz <dl035@hdm-stuttgart.de>
2014-04-28 11:42:32 +0200

    Die neuen Klassen.

---
+++ SocialMediaPinnwand/src/de/hdm/socialmediaprojekt/shared/smo/Kommentar.java
@@ -0,0 +1,49 @@
+package de.hdm.socialmediaprojekt.shared.smo;
+
+
+
+public class Kommentar extends SMObject {
+
+//  private static final long serialVersionUID = 1L;
+
+  
+  private String kommentar = "";
+  
+  private int sourceUserID = 0;
+  private int targetBeitragID = 0;
+  
+  public String getKommentar() {
+    return this.kommentar;
+  }
+
+  public void setKommentar(String kommentar) {
+    this.kommentar = kommentar;
+  }
+
+  public int getSourceUserID() {
+	    return this.sourceUserID;
+	  }
+
+		   
+  public void setSourceUserID(int sourceID) {
+	    this.sourceUserID = sourceID;
+	  }
+  
+  public int getTargetBeitragID() {
+	    return this.targetBeitragID;
+  } 
+
+		   
+public void setTargetBeitragID(int targetID){
+	    this.targetBeitragID = targetID;
+	  }
+  
+  public String toString() {
+	  	return super.toString() 
+	  	+"Der Nutzer mit der User-ID: #" + this.sourceUserID 
+	   	+ "hat den Beitrag mit der Beitrags-ID: #" + this.targetBeitragID 
+	   	+ "mit dem Kommentar:" + this.kommentar
+	   	+"kommentiert";  		
+  }
+
+}
