ec6bb1cc899490485d0c596c311860857a601074
FinkDaniel <df030@hdm-stuttgart.de>
2014-05-05 14:35:38 +0200

    all new +gui test

---
+++ SocialMediaPinnwand/src/de/hdm/socialmediaprojekt/client/Registration.java
@@ -0,0 +1,73 @@
+package de.hdm.socialmediaprojekt.client;
+
+
+import com.google.appengine.api.users.User;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.i18n.client.HasDirection.Direction;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.MouseDownHandler;
+import com.google.gwt.event.dom.client.MouseDownEvent;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.Window;
+
+import de.hdm.socialmediaprojekt.shared.smo.*;
+import de.hdm.socialmediaprojekt.server.db.*;
+
+public class Registration extends Composite {
+	FlowPanel panel = new FlowPanel();
+	
+	public Registration() {
+		panel.setPixelSize(300, 300);
+		initWidget(panel);
+		registrieren();
+		
+	}
+
+	private void registrieren() {
+		panel.clear();
+		final TextBox vorname = new TextBox();
+		vorname.setText("Vorname");
+		panel.add(vorname);
+		final TextBox nachname = new TextBox();
+		nachname.setText("Nachname");
+		panel.add(nachname);
+		final TextBox nickname = new TextBox();
+		nickname.setText("Nickname");
+		panel.add(nickname);
+		Button reg = new Button("Registrieren");
+		panel.add(reg);
+		
+		reg.addClickHandler(new ClickHandler(){
+
+			public void onClick(ClickEvent event) {
+				
+//				Anzeigen der eingegebenen Werte
+				
+				Window.alert("Folgende Angaben wurden gemacht.. \n\nVorname:" +vorname.getText()+"\nNachname:"+nachname.getText()+"\nNickname: "+nickname.getText());
+				
+//				Implementierung der Werte in die Datenbank
+				
+//				erfolgt wie im Beispiel TestUser.java
+				
+				
+				
+				
+				
+			}
+			
+		});
+		
+		
+		
+	}
+		
+}
+
